aa75917fbbbf1421a2ebaa0f21c1e52a41745bca
albus2012 <zimu_2012@126.com>
2013-12-08 15:55:25 +0800

    modify DMAC

--- ns-2.30/underwatersensor/uw_mac/DMAC/dmac.cc
+++ ns-2.30/underwatersensor/uw_mac/DMAC/dmac.cc
@@ -15,7 +15,7 @@ using namespace std;
 ofstream outfile("/home/yongj/NS2/ns-2.30/result/dmacfile");
 
 int hdr_dmac::offset_;
-Topology DMac::topo(4);
+DMac_Topology DMac::topo;
 
 static class DMACHeaderClass: public PacketHeaderClass
 {
@@ -41,52 +41,74 @@ void DMac_StatusHandler::handle(Event* e)
   mac_->StatusProcess(e);
 }
 
+void DMac_DataSendTimer::expire(Event *e)
+{
+  mac_->processDataSendTimer(this);
+}
+
+
 void DMac_WakeTimer::expire(Event *e)
 {
 
-  if(mac_->isNodeWaitFlag() || mac_->isNodeListenFlag())
-  {
-
-    mac_->setNodeSendFlag();
-    mac_->canSend();
-  }
-
-  resched(mac_->getRoundTime());
+//  if(mac_->isNodeWaitFlag() || mac_->isNodeListenFlag())
+//  {
+//
+//    mac_->setNodeSendFlag();
+//    mac_->canSend();
+//  }
+  mac_->updateNodeTime();
+  resched(mac_->getStartTime());
+  mac_->canSend();
+}
+void DMac_WakeTimer::change(double t)
+{
+  if(isPending())
+    cancel();
+  resched(t);
 }
 
 void DMac_SleepTimer::expire(Event *e)
 {
-  if(!mac_->isNodeListenFlag())
-  {
-
-    mac_->setNodeListenFlag();
-    mac_->updateNodeTime();
-  }
-
+//  if(!mac_->isNodeListenFlag())
+//  {
+//
+//    mac_->setNodeListenFlag();
+//    mac_->updateNodeTime();
+//  }
+//  mac_->updateNodeTime();
   resched(mac_->getRoundTime());
 }
 
 void DMac_StartTimer::expire(Event* e)
 {
   mac_->start();
+}
+void DMac_InitTimer::expire(Event* e)
+{
+  mac_->init();
+}
+
+
+
+DMac_Topology::DMac_Topology()
+{
 
 }
 
-Topology::Topology(int n)
-:delay(n, vector<double>(n))
+void DMac_Topology::addNode(int id, double x, double y, double z)
 {
+  Point p ={id, x, y, z};
+  outfile << "point" << p.id << p.x << p.y << p.z << endl;
+  points[id] = p;
 }
 
-void Topology::addNode(int id, int x, int y, int z)
+void DMac_Topology::setDelay()
 {
-  Point p={id, x, y, z};
-  points.push_back(p);
-}
 
-void Topology::setDelay()
-{
-  for(int i = 0; i < points.size(); i++)
+  delay.clear();
+  for(size_t i = 0; i < points.size(); i++)
   {
+    vector<double> v;
     for(int j = 0; j < points.size(); j++)
     {
       int n1 = points[i].id;
@@ -95,18 +117,31 @@ void Topology::setDelay()
       double dy = (points[i].y - points[j].y) / 1500.0;
       double dz = (points[i].z - points[j].z) / 1500.0;
 
-      delay[n1][n2] = sqrt(dx*dx + dy*dy + dz*dz);
+      v.push_back(sqrt(dx*dx + dy*dy + dz*dz));
     }
+    delay.push_back(v);
   }
 }
 
-double Topology::getDelay(int i, int j)
+double DMac_Topology::getDelay(int i, int j)
 {
   return delay[i][j];
 }
-int subtsp(vector<vector<double> >&paths, vector<int> used, int pos, vector<int>&res)
+
+double DMac_Topology::getMaxProp()
 {
-  outfile << "get";
+  double t = 0;
+  setDelay();
+  for(int i = 0; i < delay.size(); i++)
+    for(int j = i+1; j < delay.size(); j++)
+      if(t < delay[i][j])
+        t = delay[i][j];
+  return t;
+}
+
+double subtsp(vector<vector<double> >&paths, vector<int> used, int pos, vector<int>&res)
+{
+
   used.push_back(pos);
   int len = paths.size();
   if(used.size() == len)
@@ -115,7 +150,7 @@ int subtsp(vector<vector<double> >&paths, vector<int> used, int pos, vector<int>
   }
   else
   {
-    int temp = 100;
+    double temp = 10000;
     int next = 0;
     vector<int> t;
     for(int i = 0; i < len; i++)
@@ -123,7 +158,7 @@ int subtsp(vector<vector<double> >&paths, vector<int> used, int pos, vector<int>
       if(find(used.begin(), used.end() ,i) == used.end())
       {
         vector<int> v;
-        int rest = subtsp(paths,used, i,v);
+        double rest = subtsp(paths,used, i,v);
         if(rest+paths[pos][i] < temp)
         {
           temp = rest+paths[pos][i];
@@ -144,31 +179,53 @@ vector<int> tsp(vector<vector<double> >&paths)
 {
   vector<int> used;
   vector<int> res;
-  outfile <<  subtsp(paths, used, 0, res);
+  outfile << "length" << subtsp(paths, used, 0, res);
   outfile << endl;
-  for(int i = 0; i < res.size(); i++)
-    outfile << res[i];
   return res;
 }
 
-vector<int> Topology::getPath()
+vector<int> DMac_Topology::getPath()
 {
+
+  path = vector<int>(1,0);
   setDelay();
-  path = tsp(delay);
+  for(int i = 0; i < delay.size(); i++)
+  {
+    for(int j = 0; j < delay[i].size(); j++)
+    {
+      outfile << delay[i][j] << " ";
+    }
+    outfile << endl;
+  }
+  outfile << endl;
+
+  vector<int> t = tsp(delay);
+  for(int i = 0 ; i < t.size(); i++)
+    path.push_back(t[i]);
+
+  return path;
+
 }
 
-int Topology::getNextNode(int node)
+int DMac_Topology::getNextNode(int node)
 {
   int size = path.size();
   for(int i = 0; i < size; i++)
   {
     if(node == path[i])
-      return (i + 1) % size;
-    else
-      return -1;
+      return path[(i + 1) % size];
   }
 }
+int DMac_Topology::getPos(int node)
+{
+  int size = path.size();
+  for(int i = 0; i < size; i++)
+  {
+    if(node == path[i])
+      return i;
+  }
 
+}
 
 //bind the tcl object
 static class DMacClass: public TclClass
@@ -185,31 +242,45 @@ public:
 } class_dmac;
 
 
-//Time DMac::maxPropTime = UnderwaterChannel::Transmit_distance() / 1500.0;//0.7333
+//Time DMac::GuardTime = UnderwaterChannel::Transmit_distance() / 1500.0;//0.7333
 
-Time DMac::maxPropTime = 0;
-Time DMac::sendInterval = 2.0;
+Time DMac::GuardTime = 0.1;
+Time DMac::sendInterval = 1.9;
 Time DMac::baseTime = 0.2;
-int DMac::nodeCount = 8;
-
-
-
+int DMac::nodeCount = 7;
+int DMac::DataSize = 500;
+int DMac::ACKSize = 20;
 
 DMac::DMac()
     : UnderwaterMac(), callback_handler(this),
     /*pkt_send_timer(this),*/
     status_handler(this), sleep_timer(this),
-    start_timer_(this), wake_timer(this)
+    start_timer_(this), wake_timer(this),
+    init_timer_(this)
 {
   CycleCount = 1;
   NumPktSend_ = 0;
   NumDataSend_ = 0;
+  //int nodeC = 0;
+  bind("nodeCount",&nodeCount);
+  bind("sendInterval", &sendInterval);
+  bind("baseTime",&baseTime);
+  bind("guardTime",&GuardTime);
+  bind("dataSize", &DataSize);
+  bind("ackSize", &ACKSize);
+  //nodeCount = nodeC;
 
-  start_timer_.resched(0.001);
+
+  init_timer_.resched(0.05);
 
   if (!outfile)
     cout << "outfile wrong!!!";
-
+//  double X, Y, Z;    // location of this node
+    double X, Y, Z;    // location of this node
+//    if(node_)
+//    ((UnderwaterSensorNode*)node_)->getLoc(&X, &Y, &Z);
+//    topo.addNode(index_, X, Y, Z);
+//    outfile << "location" << index_ << X << Y << Z << endl;
 
 }
 
@@ -229,7 +300,7 @@ void DMac::makeData(Packet* p)
 
   hdr_cmn* cmh = HDR_CMN(p);
   cmh->direction() = hdr_cmn::DOWN;
-  cmh->txtime() = getTxTime(cmh->size());
+  cmh->txtime() = getTxDataTime(cmh->size());
 
   outfile << "DATA packet size is: " << cmh->txtime() << endl;
   hdr_mac* mh = hdr_mac::access(p);
@@ -267,26 +338,29 @@ void DMac::StatusProcess(Event *e)
 void DMac::canSend()
 {
 
-
-  setNodeSendFlag();
   Time now = Scheduler::instance().clock();
 
-  while(getNodeFlag() == NF_SEND)
-  {
 
-    if(waitingPackets_.empty() != true)
-    {
-      Packet* p = waitingPackets_.front();
-      waitingPackets_.pop_front();
-      sendData(p);
-      return;
-    }
-    else
-    {
-      setNodeNoDataFlag();
-      sendEndFlag();
-      return;
-    }
+
+  if(waitingPackets_.empty() != true)
+  {
+    Packet* p = waitingPackets_.front();
+    waitingPackets_.pop_front();
+    hdr_mac* mh = HDR_MAC(p);
+    hdr_cmn* cmh = HDR_CMN(p);
+    int dst = mh->macDA();
+
+    canSendACK(now+cmh->txtime(), dst);
+    DMac_DataSendTimer* DataSendTimer = new DMac_DataSendTimer(this);
+    DataSendTimer->pkt_ = p;
+    DataSendTimer->resched(getTxTime(ACKSize));
+    //sendData(p);
+    return;
+  }
+  else
+  {
+    canSendACK(now, BROADCAST);
+    return;
   }
 
 }
@@ -329,7 +403,8 @@ Packet* DMac::makeEndPkt()
   Packet* p = Packet::alloc();
   hdr_dmac* dh = HDR_DMAC(p);
 
-  int dst = (index_+ 1) % nodeCount;
+  //int dst = (index_+ 1) % nodeCount;
+  int dst = topo.getNextNode(index_);
 
   dh->ptype = DP_END;
   dh->ack = false;
@@ -346,6 +421,7 @@ Packet* DMac::makeEndPkt()
   cmh->addr_type() = NS_AF_ILINK;
   cmh->ptype() = PT_DMAC;
   cmh->txtime() = getTxTime(cmh->size());
+
 
 
   hdr_mac* mh = HDR_MAC(p);
@@ -404,7 +480,7 @@ void DMac::RecvProcess(Packet *p)
       << " src:" << src << endl;
 
 
-  if(dst == index_)
+  if(dst == index_ || dst == BROADCAST)
   {
 
     outfile << "DMac::RecvProcess arrive at the dest" << endl;
@@ -419,26 +495,107 @@ void DMac::handleRecvPkt(Packet* p)
   hdr_dmac* dh = HDR_DMAC(p);
 
   //the previous node has no data to send
-  if(dh->ptype == DP_END)
-  {
-    outfile << now()
-        << " # " << index_
-        << "recv end flag"
-        << endl;
-    Packet::free(p);
-    canSend();
-  }
+//  if(dh->ptype == DP_END)
+//  {
+//    outfile << now()
+//        << " # " << index_
+//        << "recv end flag"
+//        << endl;
+//    Packet::free(p);
+//    canSend();
+//  }
 
-  else if(dh->ptype == DP_DATA)
+  if(dh->ptype == DP_DATA)
   {
     handleRecvData(p);
     sendUp(p);
   }
   else if(dh->ptype == DP_ACKDATA)
   {
-    Packet::free(p);
+    handleRecvACK(p);
+    if(dh->receiver_addr == index_)
+    {
+      Packet::free(p);
+    }
     return;
   }
+}
+
+void DMac::handleRecvACK(Packet* p)
+{
+  hdr_dmac* dh = HDR_DMAC(p);
+  int dst = dh->receiver_addr;
+  double endTime = dh->t_send;
+
+
+  if(dst == index_)
+  {
+    outfile << index_ << " " << "dst recv ACK" << endl;
+    if(endTime < now())
+    {
+      wake_timer.change(0.001);
+    }
+    else
+    {
+      changeSendTime(p);
+    }
+  }
+}
+
+void DMac::changeSendTime(Packet* p)
+{
+  hdr_dmac* dh = HDR_DMAC(p);
+
+  int src = dh->sender_addr;
+
+  double endTime = dh->t_send;
+  double backoff = endTime;
+  for(int i = 0; i < nodeCount; i++)
+  {
+    double t = endTime + topo.getDelay(src,i)-topo.getDelay(i,index_);
+    if(backoff < t)
+    {
+      backoff = t;
+    }
+  }
+  wake_timer.change(backoff-now());
+
+
+}
+
+Packet* DMac::makeACK(Time t, int target)
+{
+  Time now = Scheduler::instance().clock();
+
+  Packet* q = Packet::alloc();
+  int dst = topo.getNextNode(index_);
+
+
+  hdr_cmn* cmh_q = HDR_CMN(q);
+  cmh_q->size() = ACKSize;
+  cmh_q->next_hop() = dst;
+  cmh_q->direction() = hdr_cmn::DOWN;
+  cmh_q->addr_type() = NS_AF_ILINK;
+  cmh_q->ptype() = PT_DMAC;
+  cmh_q->txtime() = getTxTime(cmh_q->size());
+  outfile << "ack packet size is:" << cmh_q->txtime() << endl;
+
+  hdr_dmac* dh_q = HDR_DMAC(q);
+  dh_q->ptype = DP_ACKDATA;
+  dh_q->pk_num = 1;    // sequence number
+  dh_q->data_num = 1;
+  dh_q->ack = false;
+  dh_q->sender_addr = index_;
+  dh_q->t_make = now;
+  dh_q->receiver_addr = dst;
+  dh_q->t_send = t + cmh_q->txtime();
+
+  hdr_mac* mh_q = HDR_MAC(q);
+  mh_q->macDA() = BROADCAST;
+  mh_q->macSA() = index_;
+
+
+  return q;
 }
 
 Packet* DMac::makeACK(Packet* p)
@@ -463,7 +620,7 @@ Packet* DMac::makeACK(Packet* p)
 
 
   hdr_cmn* cmh_q = HDR_CMN(q);
-  cmh_q->size() = hdr_dmac::size();
+  cmh_q->size() = ACKSize;//hdr_dmac::size();
   cmh_q->next_hop() = src;
   cmh_q->direction() = hdr_cmn::DOWN;
   cmh_q->addr_type() = NS_AF_ILINK;
@@ -482,9 +639,10 @@ Packet* DMac::makeACK(Packet* p)
 
 void DMac::handleRecvData(Packet* p)
 {
-  hdr_dmac* dh = HDR_DMAC(p);
-  if(dh->ack)
-    canSendACK(p);
+//  hdr_dmac* dh = HDR_DMAC(p);
+//  if(dh->ack)
+//    canSendACK(p);
+  ;
 }
 
 void DMac::canSendACK(Packet* p)
@@ -496,6 +654,12 @@ void DMac::canSendACK(Packet* p)
 
   updateNumPktSend();
 
+}
+void DMac::canSendACK(Time t, int target)
+{
+  Packet* q = makeACK(t, target);
+  sendout(q);
+  updateNumPktSend();
 }
 
 int DMac::sendout(Packet* p)
@@ -513,6 +677,7 @@ int DMac::sendout(Packet* p)
       Poweron();
 
     case IDLE:
+    case RECV:
       outfile << now()
             << " # " << index_
             << " send out"
@@ -526,17 +691,17 @@ int DMac::sendout(Packet* p)
       Scheduler::instance().schedule(&status_handler,&status_event,cmh->txtime());
       break;
 
-    case RECV:
-      outfile << now()
-            << " # " << index_
-            << " RECV-SEND Collision"
-            << " src:" << src
-            << " dest:" << dst
-            << endl;
-      printf("RECV-SEND Collision!\n");
-      Packet::free(p);
-
-      break;
+//    case RECV:
+//      outfile << now()
+//            << " # " << index_
+//            << " RECV-SEND Collision"
+//            << " src:" << src
+//            << " dest:" << dst
+//            << endl;
+//      printf("RECV-SEND Collision!\n");
+//      Packet::free(p);
+//
+//      break;
 
     default:
       //status is SEND, send too fast
@@ -579,11 +744,12 @@ void DMac::TxProcess(Packet *p)
   << " dest:" << dst
   << endl;
 
-  HDR_CMN(p)->size() = 600;
+  cmh->size() = DataSize;
   cmh->next_hop() = dst;
   cmh->direction()=hdr_cmn::DOWN;
   cmh->addr_type()=NS_AF_ILINK;
   cmh->ptype() == PT_DMAC;
+  cmh->txtime() = getTxDataTime(cmh->size());
 
   dh->ptype = DP_DATA;
 
@@ -597,31 +763,45 @@ void DMac::TxProcess(Packet *p)
       &callback_event, DMAC_CALLBACK_DELAY);
 
 }
+void DMac::init()
+{
+  double X, Y, Z;    // location of this node
+  ((UnderwaterSensorNode*)node_)->getLoc(&X, &Y, &Z);
+  topo.addNode(index_, X, Y, Z);
+  topo.getPath();
+  start_timer_.resched(0.05);
+}
 
 
 void DMac::start()
 {
   outfile << "DMac::start" << endl;
 
+//  double X, Y, Z;    // location of this node
+//  ((UnderwaterSensorNode*)node_)->getLoc(&X, &Y, &Z);
+//  topo.addNode(index_, X, Y, Z);
+//  topo.getPath();
+//  for(int i = 0; i < res.size(); i++)
+//    outfile << res[i] << endl;
   initNodeTime();
+//  double X, Y, Z;    // location of this node
+//  ((UnderwaterSensorNode*)node_)->getLoc(&X, &Y, &Z);
+//  topo.addNode(index_, X, Y, Z);
+//  outfile << "location" << index_ << X << Y << Z << endl;
 
-  topo.addPoint(0, 0, 0, 0);
-  topo.addPoint(1, 0, 1, 0);
-  topo.addPoint(2, 1, 1, 0);
-  topo.addPoint(3, 1, 0, 0);
   vector<int> res = topo.getPath();
   for(int i = 0; i < res.size(); i++)
     outfile << res[i];
   outfile << "path" << endl;
 
-  sleep_timer.resched(getInitEndTime());
-  wake_timer.resched(getInitStartTime());
+  sleep_timer.resched(getEndTime());
+  wake_timer.resched(getStartTime());
   Random::seed_heuristically();
 
 //  bind("nodeCount", &nodeCount);
 //  bind("sendInterval", &sendInterval);
 //  bind("baseTime",&baseTime);
-//  bind("maxPropTime",&maxPropTime);
+//  bind("GuardTime",&GuardTime);
 //  if(index_ ==0)
 //    canSend();
 //  MaxTxTime_ = 1610 * encoding_efficiency_ / bit_rate_;
@@ -630,24 +810,27 @@ void DMac::start()
 
 
 }
-Time DMac::getInitStartTime()
+Time DMac::getStartTime()
 {
   return nodeTime.startSend - now();
 }
 
-Time DMac::getInitEndTime()
+Time DMac::getEndTime()
 {
   return nodeTime.endSend - now();
 }
 
+
+
 Time DMac::getRoundTime()
 {
-  return nodeCount * sendInterval + (nodeCount - 1) * maxPropTime;
+  return nodeCount * sendInterval + nodeCount * GuardTime;
 }
 
 void DMac::initNodeTime()
 {
-  nodeTime.startSend = baseTime + index_ * sendInterval + index_*maxPropTime;
+
+  nodeTime.startSend = baseTime +  topo.getPos(index_)* (sendInterval + GuardTime);
   nodeTime.endSend = nodeTime.startSend + sendInterval;
 }
 
@@ -655,14 +838,23 @@ void DMac::updateNodeTime()
 {
   nodeTime.startSend += getRoundTime();
   nodeTime.endSend += getRoundTime();
+  outfile <<index_ << " " << nodeTime.startSend << " " << nodeTime.endSend << endl;
   addCycleCount();
+}
+
+void DMac::processDataSendTimer(DMac_DataSendTimer * DataSendTimer)
+{
+  DMac_DataSendTimer* tmp = DataSendTimer;
+  sendData(DataSendTimer->pkt_);
+  DataSendTimer->pkt_ = NULL;
+  delete tmp;
 }
 
 bool DMac::isSendEndFlag()
 {
 
   if(nodeTime.startSend - now() >
-        nodeCount * sendInterval - nodeCount * maxPropTime)
+        nodeCount * sendInterval - nodeCount * GuardTime)
     return false;
   else
     return true;
@@ -682,6 +874,9 @@ int DMac::command(int argc, const char * const *argv)
       if (!n1)
         return TCL_ERROR;
       node_ = n1;
+//      double X, Y, Z;
+//      ((UnderwaterSensorNode*)node_)->getLoc(&X, &Y, &Z);
+//      topo.addNode(index_, X, Y, Z);
       return TCL_OK;
     }
   }
